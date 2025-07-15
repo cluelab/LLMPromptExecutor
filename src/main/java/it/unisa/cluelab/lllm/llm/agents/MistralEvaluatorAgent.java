dee0b89b0a42a3d591887b49272df951008b1d69
Alfonso <alfonso.piscitelli91@gmail.com>
2025-07-15 12:50:40 +0200

    OLLama support for generic types

--- src/main/java/it/unisa/cluelab/lllm/llm/agents/MistralEvaluatorAgent.java
+++ src/main/java/it/unisa/cluelab/lllm/llm/agents/MistralEvaluatorAgent.java
@@ -3,12 +3,17 @@ package it.unisa.cluelab.lllm.llm.agents;
 import it.unisa.cluelab.lllm.llm.agents.generic.OLLAMAEvaluatorAgent;
 import org.json.JSONObject;
 
-public class MistralEvaluatorAgent extends OLLAMAEvaluatorAgent {
+public class MistralEvaluatorAgent extends OLLAMAEvaluatorAgent<String> {
 
 
     public MistralEvaluatorAgent(JSONObject setttings) {
         super(setttings);
         setModel("mistral");
+    }
+
+    @Override
+    public String parse(String rawContent) {
+        return rawContent;
     }
 
 }
